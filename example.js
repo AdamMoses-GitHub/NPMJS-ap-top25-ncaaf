40bb5a685a0a130ead127becbfb54157e7222979
Adam Moses <adammoses@adammoses.com>
2018-08-06 16:39:22 -0400

    fixed calling function

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ var aptop25ncaaf = require('ap-top25-ncaaf');
 
 console.log('Usage example of the ap-top25-ncaaf package.')
 
-aptop25ncaaf.getAPTop25Data( function(error, data) {
+aptop25ncaaf.getAPTop25NCAAFRankingsData( function(error, data) {
         if (!error) {
             console.log(JSON.stringify(data, null, 2));      
         }
